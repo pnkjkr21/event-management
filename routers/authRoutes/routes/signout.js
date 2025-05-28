b99f1033ca3b989b6fccf2ed5f89d9ea8b9dacf1
pnkrkr21 <pankjkr2014@gmail.com>
2025-05-29 02:34:03 +0530

    first checking if user already logged in before signing out

--- routers/authRoutes/routes/signout.js
+++ routers/authRoutes/routes/signout.js
@@ -1,3 +1,9 @@
 module.exports = async ( req, res ) => {
+    const authToken = req.headers.authorization
+    if (!authToken) {
+        return res.status(400).json({
+            message: "Already logged out"
+        })
+    }
     return res.clearCookie('Authorization').status(200).json({ success: true, message: "Signed Out Successfully" })
 }
